88c0e1c0ec041c5563638a44e4ec926766806d9a
Mikhail Baryshnikov <dopamine.flint@gmail.com>
2018-11-08 23:30:38 +0300

    Lesson1

---
+++ triangle.rb
@@ -0,0 +1,17 @@
+puts "Площадь треугольника "
+
+sleep 1
+
+puts "Введите основание треугольника: "
+a = gets.chomp.to_f
+
+sleep 1
+
+puts "Введите высоту: "
+h = gets.chomp.to_f
+b = 1.to_f
+c = 2.to_f
+
+s = (b/c)*a*h
+
+puts s

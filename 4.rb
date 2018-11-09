e4f0b4bc8a8c4e27ad7b0b52e1f1c6c6fb2b5748
Mikhail Baryshnikov <dopamine.flint@gmail.com>
2018-11-10 02:52:07 +0300

    Lesson1 fixed

--- 4.rb
+++ 4.rb
@@ -5,7 +5,7 @@ b = gets.chomp.to_f
 puts "Введите третье число"
 c = gets.chomp.to_f
 
-d = (b**2) - 4*a*c
+d = b**2 - 4*a*c
 
 x1 = 0
 x2 = 0
@@ -16,8 +16,11 @@ puts " "
 if d < 0
 	puts "Корней нет!"
 elsif d == 0
-	puts "Корень = #{x1 = (-b + Math.sqrt(d)) / 2*a}"
+	puts "Корень = #{-b / 2 * a}"
 else 
-	puts "1-ый корень = #{x1 = (-b + Math.sqrt(d)) / 2*a}"
-	puts "2-ой корень = #{x1 = (-b - Math.sqrt(d)) / 2*a}"
+	sqrt = Math.sqrt(d)
+  x1 = (-b + sqrt) / 2 * a
+  x2 = (-b - sqrt) / 2 * a
+	puts "1-ый корень = #{x1}"
+	puts "2-ой корень = #{x2}"
 end

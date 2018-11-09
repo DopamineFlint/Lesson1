5e037877c44df5aef94ab2b556d4e640b16d985e
Mikhail Baryshnikov <dopamine.flint@gmail.com>
2018-11-10 02:49:08 +0300

    Lesson1 fixed

--- Ideal_Weight.rb
+++ Ideal_Weight.rb
@@ -2,16 +2,14 @@ puts "What's your name? "
 
 name = gets.chomp
 
-sleep 1
-
 puts "What's your height in centimetres? "
 
 height = gets.chomp.to_i
 
-height = height - 110
+height_index = height - 110
 
-if height <= 0
+if height_index <= 0
 	puts "#{name}, your weight is optimal"
 else
-	puts "#{name}, your ideal weight is #{height.to_s} kgs"
+	puts "#{name}, your ideal weight is #{height_index} kgs"
 end

448aa827c7241938011461722782647a2f85464c
Mikhail Baryshnikov <dopamine.flint@gmail.com>
2018-11-10 02:51:22 +0300

    Lesson1 fixed

--- triangle.rb
+++ triangle.rb
@@ -1,17 +1,11 @@
 puts "Площадь треугольника "
 
-sleep 1
-
 puts "Введите основание треугольника: "
 a = gets.chomp.to_f
 
-sleep 1
-
 puts "Введите высоту: "
 h = gets.chomp.to_f
-b = 1.to_f
-c = 2.to_f
 
-s = (b/c)*a*h
+s = 0.5 * a * h
 
 puts s

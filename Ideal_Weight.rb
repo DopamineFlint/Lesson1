be8768a20b9a85f0bae5da4ef66a6ae98bfa6250
Mikhail Baryshnikov <dopamine.flint@gmail.com>
2018-11-12 16:02:14 +0300

    Lesson1 fixed

--- Ideal_Weight.rb
+++ Ideal_Weight.rb
@@ -9,7 +9,7 @@ height = gets.chomp.to_i
 height_index = height - 110
 
 if height_index <= 0
-	puts "#{name}, your weight is optimal"
+  puts "#{name}, your weight is optimal"
 else
-	puts "#{name}, your ideal weight is #{height_index} kgs"
+  puts "#{name}, your ideal weight is #{height_index} kgs"
 end
